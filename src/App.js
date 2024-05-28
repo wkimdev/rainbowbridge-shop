2c752f44f7cd2ac4472101fe2e1839281db9fd05
wkimdev <wkimdev@gmail.com>
2024-05-28 15:59:57 +0900

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React-멋져!!!
         </a>
       </header>
     </div>
